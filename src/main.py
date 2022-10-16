2f5b8274cfd54d2e523057d8979cc13cbd843e3b
kunimasu <kunimasu@kunimasu.com>
2022-10-16 12:04:58 +0900

    add json headers

--- src/main.py
+++ src/main.py
@@ -1,17 +1,20 @@
 import os
-import requests
 from typing import Final
+
+import requests
 
 KUNIMASU_URL: Final[str] = os.environ['KUNIMASU_URL']
 
 def main():
     if all([KUNIMASU_URL]):
-        result: Final[requests.Response] = requests.post(KUNIMASU_URL)
+        post_data = {
+            'type': 123
+        }
+        result: Final[requests.Response] = requests.post(KUNIMASU_URL, json=post_data, headers={"test": "hige"})
         print(str(result.text))
         # print(result.json()["result"])
     else:
         print('Error: app information not configured')
-
 
 if __name__ == '__main__':
     main()
