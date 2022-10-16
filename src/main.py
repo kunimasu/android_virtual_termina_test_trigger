09abc718b07384ac6b8a3765783f703cb8202890
kunimasu <kunimasu@kunimasu.com>
2022-10-16 11:01:03 +0900

    result is str2

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ KUNIMASU_URL: Final[str] = os.environ['KUNIMASU_URL']
 def main():
     if all([KUNIMASU_URL]):
         result: Final[requests.Response] = requests.post(KUNIMASU_URL)
-        print(str(result.raw))
+        print(str(result.text))
         # print(result.json()["result"])
     else:
         print('Error: app information not configured')
