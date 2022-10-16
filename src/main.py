9d6ccd16f6dd010e99a8ace073ca3612e7996ab4
kunimasu <kunimasu@kunimasu.com>
2022-10-16 19:34:51 +0900

    post repo

--- src/main.py
+++ src/main.py
@@ -6,14 +6,17 @@ import requests
 KUNIMASU_URL: Final[str] = os.environ['KUNIMASU_URL']
 REPO_NAME: Final[str] = os.environ['REPO_NAME']
 REPO_OWNER: Final[str] = os.environ['REPO_OWNER']
+REPO_HEAD_SHA: Final[str] = os.environ['REPO_HEAD_SHA']
 
 def main():
     if all([KUNIMASU_URL]):
         post_data = {
-            'type': 123
+            'repo_name': REPO_NAME,
+            'repo_owner': REPO_OWNER,
+            'repo_head_sha': REPO_HEAD_SHA
         }
-        result: Final[requests.Response] = requests.post(KUNIMASU_URL, json=post_data, headers={"test": "123"})
-        print(str(result.text), REPO_NAME, REPO_OWNER, "end")
+        result: Final[requests.Response] = requests.post(KUNIMASU_URL, json=post_data)
+        print(str(result.text), REPO_NAME, REPO_OWNER, REPO_HEAD_SHA, "end")
         # print(result.json()["result"])
     else:
         print('Error: app information not configured')
