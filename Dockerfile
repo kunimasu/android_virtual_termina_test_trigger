3aba6398bd5baf3d8b8c8aaf9c2ff9cb0abe9457
kunimasu <kunimasu@kunimasu.com>
2022-10-16 09:43:24 +0900

    first

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10
+
+WORKDIR /app
+COPY requirements.txt /app
+RUN pip install --upgrade pip && pip install -r /app/requirements.txt
+
+COPY src/ /app
+
+CMD ["python3", "/app/src/main.py"]
---
+++ src/main.py
@@ -0,0 +1,16 @@
+import os
+import requests
+from typing import Final
+
+KUNIMASU_URL: Final[str] = os.environ['KUNIMASU_URL']
+
+def main():
+    if all([KUNIMASU_URL]):
+        result: Final[requests.Response] = requests.post(KUNIMASU_URL)
+        print(result.json()["result"])
+    else:
+        print('Error: app information not configured')
+
+
+if __name__ == '__main__':
+    main()
