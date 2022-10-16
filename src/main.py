dfc7b20eb62e15f5be0283241a137101f041619b
kunimasu <kunimasu@kunimasu.com>
2022-10-16 19:03:53 +0900

    debug

--- src/main.py
+++ src/main.py
@@ -4,14 +4,16 @@ from typing import Final
 import requests
 
 KUNIMASU_URL: Final[str] = os.environ['KUNIMASU_URL']
+REPO_NAME: Final[str] = os.environ['REPO_NAME']
+REPO_OWNER: Final[str] = os.environ['REPO_OWNER']
 
 def main():
     if all([KUNIMASU_URL]):
         post_data = {
             'type': 123
         }
-        result: Final[requests.Response] = requests.post(KUNIMASU_URL, json=post_data, headers={"test": "hige"})
-        print(str(result.text))
+        result: Final[requests.Response] = requests.post(KUNIMASU_URL, json=post_data, headers={"test": "123"})
+        print(str(result.text), REPO_NAME, REPO_OWNER, "end")
         # print(result.json()["result"])
     else:
         print('Error: app information not configured')
