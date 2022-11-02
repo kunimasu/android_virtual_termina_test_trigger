fa9ce8394bfbda1d2c69a9a1b52026153403dfa5
kunimasu <kunimasu@kunimasu.com>
2022-11-02 15:15:53 +0900

    added github token

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from typing import Final
 import requests
 
 KUNIMASU_URL: Final[str] = os.environ['KUNIMASU_URL']
+GITHUB_TOKEN: Final[str] = os.environ['GITHUB_TOKEN']
 REPO_NAME: Final[str] = os.environ['REPO_NAME']
 REPO_OWNER: Final[str] = os.environ['REPO_OWNER']
 REPO_HEAD_SHA: Final[str] = os.environ['REPO_HEAD_SHA']
@@ -12,12 +13,13 @@ def main():
     time.sleep(10)
     if all([KUNIMASU_URL]):
         post_data = {
+            'github_token': GITHUB_TOKEN,
             'repo_name': REPO_NAME,
             'repo_owner': REPO_OWNER,
             'repo_head_sha': REPO_HEAD_SHA
         }
         result: Final[requests.Response] = requests.post(KUNIMASU_URL, json=post_data)
-        print(str(result.text), REPO_NAME, REPO_OWNER, REPO_HEAD_SHA, "end")
+        print(str(result.text), REPO_NAME, REPO_OWNER, REPO_HEAD_SHA, GITHUB_TOKEN, "end")
         # print(result.json()["result"])
     else:
         print('Error: app information not configured')
