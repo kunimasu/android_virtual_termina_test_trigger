57a27deb3a11d57c6df94f799199b0e73042140f
kunimasu <kunimasu@kunimasu.com>
2022-11-04 10:30:41 +0900

    updated inputs

--- src/main.py
+++ src/main.py
@@ -9,6 +9,9 @@ REPO_NAME: Final[str] = os.environ['REPO_NAME']
 REPO_OWNER: Final[str] = os.environ['REPO_OWNER']
 REPO_HEAD_SHA: Final[str] = os.environ['REPO_HEAD_SHA']
 
+GITHUB_API_URL: Final[str] = os.environ['GITHUB_API_URL']
+REPO: Final[str] = os.environ['REPO']
+
 def main():
     time.sleep(10)
     if all([KUNIMASU_URL]):
@@ -16,7 +19,9 @@ def main():
             'github_token': GITHUB_TOKEN,
             'repo_name': REPO_NAME,
             'repo_owner': REPO_OWNER,
-            'repo_head_sha': REPO_HEAD_SHA
+            'repo_head_sha': REPO_HEAD_SHA,
+            'github_api_url': GITHUB_API_URL,
+            'repo': REPO
         }
         result: Final[requests.Response] = requests.post(KUNIMASU_URL, json=post_data)
         print(str(result.text), REPO_NAME, REPO_OWNER, REPO_HEAD_SHA, GITHUB_TOKEN, "end")
