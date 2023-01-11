cba3652ededd6e99fda30d6720d261c9d2a9c2bf
kunimasu <kunimasu@kunimasu.com>
2023-01-11 12:05:34 +0900

    現行仕様に整理

--- src/test_plan.py
+++ src/test_plan.py
@@ -16,36 +16,45 @@ class TestPlanStep:
         }
 
 class TestPlan:
-    version: Final[str]
-    title: Final[str]
+    format_version: Final[str]
     device: Final[str]
     os_version: Final[str]
+    languages: Final[List[str]]
     steps: Final[List[TestPlanStep]]
 
-    def __init__(self, version: str, title: str, device: str, os_version: str, steps: List[TestPlanStep]):
-        self.version = version
-        self.title = title
+    def __init__(
+        self,
+        format_version: str,
+        device: str,
+        os_version: str,
+        languages: List[str],
+        steps: List[TestPlanStep]
+    ):
+        self.format_version = format_version
         self.device = device
         self.os_version = os_version
+        self.languages = languages
         self.steps = steps
 
     @staticmethod
     def create(obj: any):
+        format_version = obj['format-version']
+        device = obj['device']
+        os_version = obj['os-version']
+        languages = obj['languages']
         steps: List[TestPlanStep] = []
         for step in obj['steps']:
             for action, param in step.items():
                 steps.append(TestPlanStep(action, param))
-        device = obj['device']
-        os_version = obj['os-version']
-        return TestPlan(obj['version'], obj.get('title', f'${device}(${os_version})'), device, os_version, steps)
+        return TestPlan(format_version, device, os_version, languages, steps)
 
     @property
     def dict(self) -> dict:
         map_handler: Callable[[TestPlanStep], dict] = lambda x: x.dict
         return {
-            'version': self.version,
-            'title': self.title,
+            'format_version': self.format_version,
             'device': self.device,
             'osVersion': self.os_version,
+            'languages': self.languages,
             'steps': list(map(map_handler, self.steps)),
         }
