1b2e145b02f234a0f5cc2dcc58160341922e65dd
kunimasu <kunimasu@kunimasu.com>
2023-01-11 20:43:12 +0900

    remove comment

--- src/main.py
+++ src/main.py
@@ -17,7 +17,6 @@ def main():
             test_plans.append(TestPlan.create(yaml.safe_load(f)))
     map_handler: Callable[[TestPlan], dict] = lambda x: x.dict
     sys.stdout.write(json.dumps(list(map(map_handler, test_plans))))
-    # .replace("\"", "\\\""))
 
 if __name__ == '__main__':
     main()
